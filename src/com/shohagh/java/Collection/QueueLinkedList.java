d6460f93b229577c3f74d60b38f94519c4a1a169
Md. Ahsan Kabir <ahsankabircse@gmail.com>
2017-10-17 17:48:55 +0600

    Queue test;

---
+++ src/com/shohagh/java/Collection/QueueLinkedList.java
@@ -0,0 +1,32 @@
+package com.shohagh.java.Collection;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class QueueLinkedList {
+	public static void main(String[] args) {
+		Queue<String> queue = new LinkedList<>();
+		queue.add("shohagh");
+		queue.add("Amit");  
+		queue.add("Vijay");  
+		queue.add("Karan");  
+		queue.add("Jai");  
+		queue.add("Rahul");  
+		System.out.println("head:"+queue.element());  
+		System.out.println("head:"+queue.peek());  
+		System.out.println("iterating the queue elements:");  
+		Iterator itr=queue.iterator();  
+		while(itr.hasNext()){  
+		System.out.println(itr.next());  
+		}  
+		queue.remove();  
+		queue.poll();  
+		System.out.println("after removing two elements:");  
+		Iterator<String> itr2=queue.iterator();  
+		while(itr2.hasNext()){  
+		System.out.println(itr2.next());  
+		}  
+	}
+}
