027f1e1310534323ec82d1a13f2099f3878ca78d
Md. Ahsan Kabir <ahsankabircse@gmail.com>
2018-03-28 15:46:04 +0600

    Hash map print test

---
+++ src/com/shohagh/java/Collection/HashMapTest.java
@@ -0,0 +1,15 @@
+package com.shohagh.java.Collection;
+
+import java.util.HashMap;
+
+public class HashMapTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap hm = new HashMap();
+		hm.put("test", "ami");
+		hm.put("test2", "tumi");
+		System.out.println(hm);
+	}
+
+}
